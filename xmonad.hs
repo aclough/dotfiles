907cdd6306d5afef3d9d7f580368b7ef9a02caf2
Andrew Clough <andrew.clough@vecna.com>
2012-11-09 11:14:22 -0500

    Further work on xmonad.hs
    I chnaged the layout order, added minimize, and added an event hook
    to let me minimize windows from GNOME

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,13 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.Minimize
 import XMonad.Util.EZConfig
 import XMonad.Actions.CycleWS
 import System.IO
 import XMonad.Actions.Submap
+import XMonad.Layout.Minimize
+import XMonad.Layout.LimitWindows
 import qualified XMonad.StackSet as W
 
 myWorkspaces = map show [1..9]
@@ -13,20 +16,28 @@ myManageHook :: [ManageHook]
 myManageHook =
         [ resource =? "Do"  --> doIgnore ]
 
+myLayout =  minimize (avoidStruts ( tiled ||| Full |||  Mirror tiled ))
+  where
+    tiled = limitWindows 6 $ Tall 1 0.03 0.5
+
+myHandleEventHook = minimizeEventHook
+
 main = xmonad $ gnomeConfig
     { terminal = "terminator"
     , modMask = mod4Mask -- use the mod key to the windows key
     , manageHook = manageHook gnomeConfig <+> composeAll myManageHook 
+    , layoutHook = myLayout
+    , handleEventHook = myHandleEventHook
     }
     `additionalKeysP`(
         [ ("M-S-q", spawn "gnome-session-save --gui --logout-dialog")
         , ("M-c", kill)
-        , ("M-M1-p", spawn "yeganesh -")
-        , ("M-n", spawn "yeganesh -x")
-        , ("M-p", spawn "gnome-do")
+        , ("M-n", spawn "gnome-do")
         , ("M-;", spawn "terminator")
         , ("M-b", spawn "google-chrome")
         , ("M-v", spawn "nautilus ~")
+        , ("M-m", withFocused minimizeWindow)
+        , ("M-M1-m", sendMessage RestoreNextMinimizedWin)
         , ("M-u", prevWS)
         , ("M-i", nextWS)
         , ("M-S-u", shiftToPrev)
