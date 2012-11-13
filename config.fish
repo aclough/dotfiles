a9d12b02d3e5103f7a7d19693cc829d6d2092e08
Andrew Clough <andrew.clough@vecna.com>
2012-11-13 09:22:21 -0500

    Added fish config file.

---
+++ config.fish
@@ -0,0 +1,20 @@
+set fish_git_dirty_color red
+function parse_git_dirty
+         git diff --quiet HEAD ^&-
+         if test $status = 1
+            echo (set_color $fish_git_dirty_color)"Δ"(set_color normal)
+         end
+end
+function parse_git_branch
+         # git branch outputs lines, the current branch is prefixed with a *
+         set -l branch (git branch --color ^&- | awk '/\*/ {print $2}') 
+         echo $branch (parse_git_dirty)
+end
+
+function fish_prompt
+         if test -z (git branch --quiet 2>| awk '/fatal:/ {print "no git"}')
+            printf '%s@%s %s%s%s (%s) $ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+         else
+            printf '%s@%s %s%s%s $ '  (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+         end
+end
