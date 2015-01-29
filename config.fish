5799bb143252edce5a34553f3e97913b8c843893
Andrew Clough <andrew.clough@vecna.com>
2015-01-29 13:40:57 -0500

    ROS works great now without bash recourse

--- config.fish
+++ config.fish
@@ -10,6 +10,7 @@ set -x JAVA_OPTS "-server -Xmx1024m -Xms512m -XX:MaxPermSize=512m"
 set -x M2_HOME /opt/maven
 set -x M2 /opt/maven/bin
 set -x MAVEN_HOME /opt/maven
+set -x MAVENREPO_PATH /other/maven/mavenRepository
 
 set -x GIT_CEILING_DIRECTORIES "/net/home3:/net/home4:/net/home:/net/home2:/net"
 
@@ -29,23 +30,26 @@ set -x NRV_6DOF_PATH /other/workspace/nrv-clean/6dof
 set -x NRV_RPR_PATH /other/workspace/nrv-clean/rpr
 set -x NRV_CORE_PATH /other/workspace/nrv-clean/core/
 
-set -x VROS_PATH /other/workspace/vros/devel
+set -x VROS_PATH /other/workspace/vros
+set -x ROS_WORKSPACE $VROS_PATH
 set -x ROS_DISTRO indigo
-set -x ROS_HOME /other/logs/.ros
+set -x ROS_HOME /other/logs/ros
 set ROSPATH /opt/ros/$ROS_DISTRO
 set -x ROS_ROOT $ROSPATH/share/ros
-set -x CMAKE_PREFIX_PATH $VROS_PATH:$ROSPATH
-set -x LD_LIBRARY_PATH VROS_PATH/lib:$VROS_PATH/lib/x86_64-linux-gnu:$ROSPATH/lib:$ROSPATH/lib/x86_64-linux-gnu:/other/libhid/src/.libs
-#set -x PKG_CONFIG $ROSPATH/lib/pkgconfig:$ROSPATH/pkgconfig/x86_64-linux-gnu/pkgconfig
+
+set -x ROS_PACKAGE_PATH /other/workspace/vros/src:$ROSPATH/share:$ROSPATH/stacks
+set -x ROS_SHARE_PATH $VROS_PATH/devel/share:$ROS_PACKAGE_PATH
+set -x CMAKE_PREFIX_PATH $VROS_PATH/devel:$ROSPATH
+set -x LD_LIBRARY_PATH VROS_PATH/devel/lib:$VROS_PATH/devel/lib/x86_64-linux-gnu:$ROSPATH/lib:$ROSPATH/lib/x86_64-linux-gnu:/other/libhid/src/.libs
 set -x INCLUDE_PATH /usr/include/eigen3:$ROSPATH/include:$ROSPATH/include/opencv
 set -x CPLUS_INCLUDE_PATH $INCLUDE_PATH
-set -x CPATH $INCLUDE_PATH:$VROS_PATH/include
-set -x PYTHONPATH $VROS_PATH/lib/python2.7/dist-packages:$ROSPATH/lib/python2.7/dist-packages
-set -x ROS_WORKSPACE /other/workspace/vros
-set -x ROSLISP_PACKAGE_DIRECTORIES $ROSPATH/share/common-lisp/ros:$VROS_PATH/share/common-lisp
+set -x CPATH $INCLUDE_PATH:$VROS_PATH/devel/include
+set -x PYTHONPATH $VROS_PATH/devel/lib/python2.7/dist-packages:$ROSPATH/lib/python2.7/dist-packages
+set -x ROSLISP_PACKAGE_DIRECTORIES $ROSPATH/share/common-lisp/ros:$VROS_PATH/devel/share/common-lisp
 set -x ROS_ETC_DIR /opt/ros/indigo/etc/ros
 set -x ROS_MASTER_URI http://localhost:11311
-set -x ROS_PACKAGE_PATH /other/workspace/vros/src:$ROS_HOME/share:$ROS_HOME/stacks
+set -x ROS_MAVEN_DEPLOYMENT_REPOSITORY $MAVENREPO_PATH
+set -x ROS_MAVEN_PATH $ROS_MAVEN_DEPLOYMENT_REPOSITORY:$ROS_MAVEN_PATH
 
 set -x PATH $NRV_QCBOT_PATH/scripts $HOME/bin /opt/ros/indigo/bin $JAVA_HOME/bin $M2 $PATH
 
