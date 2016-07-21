539358cb552444b4c01468e5a44d8fbfea43e58b
Andrew Clough <andrew.clough@gmail.com>
2016-07-21 14:57:48 -0400

    Add screenrc

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ sudo pip install sympy
 
 # Take the stuff from this dotfiles folder (that I care about) and symlink it
 ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/screenrc ~/.screenrc
 mkdir ~/.xmonad
 ln -s ~/dotfiles/xmonad.hs ~/.xmonad
 mkdir ~/.config/fish
