d4fdb5f3ebb225f43ff262dbe18fcff8e584bd89
Andrew Clough <andrew.clough@gmail.com>
2016-11-25 16:59:02 -0500

    Simplify fish config

--- config.fish
+++ config.fish
@@ -3,21 +3,6 @@ set -x EDITOR vim
 set PATH $PATH $HOME/bin
 set fish_git_dirty_color red
 
-set -x ROS_DISTRO indigo
-set -x ROSPATH /opt/ros/$ROS_DISTRO
-set -x ROS_ROOT $ROSPATH/share/ros
-set -x ROS_ETC_DIR $ROSPATH/etc/ros
-set -x ROS_WORKSPACE /home/aclough/ros
-set -x ROS_PACKAGE_PATH $ROSPATH/share:$ROSPATH/stacks:$ROS_WORKSPACE/share  # not in rosbash
-set -x ROS_MASTER_URI http://localhost:11311
-set -x CMAKE_PREFIX_PATH $ROSPATH
-set -x CPATH $ROSPATH/include
-set -x LD_LIBRARY_PATH $ROSPATH/lib:$ROSPATH/lib/x86_64-linux-gnu
-set PATH $PATH $ROSPATH/bin
-set -x PKG_CONFIG $ROSPATH/lib/pkgconfig:$ROSPATH/lib/x86_64-linux-gnu/pkgconfig
-set -x PYTHONPATH $ROSPATH/lib/python2.7/dist-packages
-
-. ~/dotfiles/rosfish
 
 function parse_git_dirty
          git diff --quiet HEAD ^&-
@@ -57,4 +42,25 @@ end
 
 function fng
     find -name $argv[1] -print0 2> /dev/null | xargs -0 grep $argv[2]
+end
+
+function a
+    ag --color-line-number '0;32' --color-match '0;31' --color-path '0;33' $argv
+end
+
+function v
+    vim -p $argv
+end
+
+# Ubuntu .bashrc compatibility
+function ll
+    ls -alF
+end
+
+function la
+    ls -A
+end
+
+function l
+    ls -CF
 end
