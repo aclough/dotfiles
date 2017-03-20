dec2b78c14d0bcd31b049f6da588e465d89c1e2e
Andrew Clough <andrew.clough@gmail.com>
2017-03-20 10:44:18 -0400

    More files to blow away

--- pickb.sh
+++ pickb.sh
@@ -2,4 +2,6 @@
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_aardvark.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eurosort.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
+rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
+rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
 ~/rhr/pick/software/build.sh build
