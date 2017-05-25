2848f72c0c837c753a07bf4e038f4faed27a9f52
Andrew Clough <andrew.clough@gmail.com>
2017-05-25 15:56:31 -0400

    Always addmyself to dialout on my main machine

--- main_computer_setup.sh
+++ main_computer_setup.sh
@@ -10,6 +10,8 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
+sudo addgroup aclough dialout
+
 # Remove the shopping lense
 sudo apt-get remove unity-lens-shopping
 
