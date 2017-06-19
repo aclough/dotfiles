db8b7218fd543a082cba2e36294dbfe288075109
Andrew Clough <andrew.clough@gmail.com>
2017-06-19 16:04:45 -0400

    Get rid of Julia json too.

--- pickbc.sh
+++ pickbc.sh
@@ -5,4 +5,5 @@ rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
+rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
 ~/rhr/pick/software/build.sh build
