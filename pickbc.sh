b65e1e9e85c5bf6b4a0dbb03efc347a4d8582e17
Andrew Clough <andrew.clough@gmail.com>
2017-10-05 16:17:00 -0400

    Remove another file on clean build

--- pickbc.sh
+++ pickbc.sh
@@ -6,5 +6,6 @@ rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
+rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
