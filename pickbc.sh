8cd28cdd28d7c01a8449eb51fdacc6698b4f5232
Andrew Clough <andrew.clough@gmail.com>
2017-10-06 09:46:37 -0400

    Add another thing to remove

--- pickbc.sh
+++ pickbc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_aardvark.json
+rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eurosort.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
 rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
