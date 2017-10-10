89841dd56f9d52ea501058dbc4cf720c1ea74a69
Andrew Clough <andrew.clough@gmail.com>
2017-10-10 09:17:37 -0400

    But don't error on nonexisting coordinate frames

--- pickbc.sh
+++ pickbc.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_aardvark.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eurosort.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
-rm /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_aardvark.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eurosort.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
