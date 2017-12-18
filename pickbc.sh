74345921de6f927585cc14f8d14c3971db55dabe
Andrew Clough <andrew.clough@gmail.com>
2017-12-18 13:23:39 -0500

    Add Chappie

--- pickbc.sh
+++ pickbc.sh
@@ -11,5 +11,6 @@ rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_chappie.json
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
