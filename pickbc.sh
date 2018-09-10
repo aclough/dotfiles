7d51225fbb213d847e797cc7d969f362f1167d43
Andrew Clough <andrew.clough@gmail.com>
2018-09-10 13:31:41 -0400

    Update coordinate frames file

--- pickbc.sh
+++ pickbc.sh
@@ -3,14 +3,14 @@
 set -e
 unset PYTHONPATH
 
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_abba_full_pallet.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_abba_half_pallet.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_chappie.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_distrisort.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_theopolis.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_modex_abba.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_theopolis.json
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
