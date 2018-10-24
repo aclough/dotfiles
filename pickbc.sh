4df4c90ef88d0a37bbbf1a5d2c198606c0ca4895
Andrew Clough <andrew.clough@gmail.com>
2018-10-24 09:53:45 -0400

    New coordinate frames file

--- pickbc.sh
+++ pickbc.sh
@@ -12,6 +12,7 @@ rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_modex_abba.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_sam.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_theopolis.json
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
