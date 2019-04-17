323b34ad31ab5a354a94a09d21bce86870199868
Andrew Clough <andrew.clough@gmail.com>
2019-04-17 12:09:42 -0400

    Simplify cleanup of coordinate frames

--- pickbc.sh
+++ pickbc.sh
@@ -4,24 +4,6 @@ set -e
 unset PYTHONPATH
 
 rm -f /home/aclough/rhr/pick/build/deploy/config/arm_links.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/default_planner_parameters.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/default_workspace_obstacles.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_abba_full_pallet.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_abba_full_pallet_ur10e.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_abba_half_pallet.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_bishop.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_bender.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_chappie.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_distrisort.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_johnny5.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_mission_control_tests.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_mission_control_zone_mapping_test.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_mission_example_tests.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_modex_abba.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_sam.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_theopolis.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_*
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
--- pickbc_plus.sh
+++ pickbc_plus.sh
@@ -5,15 +5,7 @@ unset PYTHONPATH
 
 sudo service memcachedb restart
 
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_aardvark.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eyehand.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_eurosort.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_hbc.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_accutech.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_promat.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_julia.json
-rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_twoeyes.json
+rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_*
 
 cd ~/rhr/pick
 rm -r build/*
