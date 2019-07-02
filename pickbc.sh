991c5fd5fbf42220d8eb69cd590f3b3fcf1a8826
Andrew Clough <andrew.clough@gmail.com>
2019-07-02 10:41:06 -0400

    More extra files to remove

--- pickbc.sh
+++ pickbc.sh
@@ -5,6 +5,7 @@ unset PYTHONPATH
 
 rm -f /home/aclough/rhr/pick/build/deploy/config/arm_links.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/coordinate_frames_*
+rm -f /home/aclough/rhr/pick/build/deploy/config/*planner_parameters.json
 rm -f /home/aclough/rhr/pick/build/deploy/config/default_*
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/build.sh runtests
