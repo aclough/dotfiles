dd858a456607bb997efcc8cd016cfae7d28f2b98
Andrew <andrew.clough@gmail.com>
2020-06-13 11:44:45 -0400

    Modernize fish usage, assume nvim

--- config.fish
+++ config.fish
@@ -12,14 +12,14 @@ set PATH $PATH $HOME/.local/bin $HOME/.cargo/bin
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 
 function parse_git_dirty
-         git diff --quiet HEAD ^&-
+         git diff --quiet HEAD 2>&-
          if test $status = 1
             echo (set_color $fish_git_dirty_color)"Δ"(set_color normal)
          end
 end
 function parse_git_branch
          # git branch outputs lines, the current branch is prefixed with a *
-         set -l branch (git branch --color ^&- | awk '/\*/ {print $2}')
+         set -l branch (git branch --color 2>&- | awk '/\*/ {print $2}')
          echo $branch (parse_git_dirty)
 end
 
@@ -54,27 +54,11 @@ function a
 end
 
 function v
-    if type -p nvim
-        nvim -p $argv
-    else
-        vim -p $argv
-    end
+    nvim -p $argv
 end
 
 function vd
-    if type -p nvim
-        nvim -d $argv
-    else
-        vimdiff  $argv
-    end
-end
-
-function vdiff
-    if type -p nvim
-        nvim -d $argv
-    else
-        vimdiff $argv
-    end
+    nvim -d $argv
 end
 
 function rp
