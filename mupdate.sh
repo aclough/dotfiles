53b9f78f6f865967e7f72756dff7266ab5174ff0
Andrew Clough <andrew.clough@gmail.com>
2020-06-30 21:31:35 -0400

    Regenerate Cuda link
    Having both Cuda 8 and 10 installed can cause the /usr/lib/cuda softlink
    to sometimes not point to either after updates or autoremove or
    something.  So make sure to regenerate it on a mupdate if it's missing.

--- mupdate.sh
+++ mupdate.sh
@@ -5,6 +5,10 @@ git pull
 /srv/package-mirror/mirror_pull
 sudo apt update
 sudo apt-get dist-upgrade
+# If soemthing has removed the cuda link regenerate it
+if ! test -L /usr/local/cuda; then
+    sudo apt install --reinstall cuda-toolkit-10-2
+fi
 bounce --inject-ssh-config
 fish -c fish_update_completions
 tldr --update
