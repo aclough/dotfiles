0d324654289985afc71e77d5d74559809b0aa296
Andrew <andrew.clough@gmail.com>
2021-07-05 15:25:02 -0400

    Add advanced radeon tools
    * Oibaf up to date Mesa, etc
    * Radeon profile tool

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ set -e
 # sudo apt-add-repository ppa:oibaf/graphics-drivers
 # sudo add-apt-repository ppa:kisak/kisak-mesa
 # For AMD power management
+# sudo add-apt-repository ppa:radeon-profile/radeon-profile
+# sudo add-apt-repository ppa:oibaf/graphics-drivers
+# sudo apt update
+# sudo apt install radeon-profile
 # Sadly 20.04 only for now
 # sudo add-apt-repository ppa:slimbook/slimbook
 # sudo apt install slimbookamdcontroller
