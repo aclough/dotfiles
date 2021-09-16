5c0ba6d215ebee4af5532d124182c648f1929a3a
Andrew Clough <andrew.clough@gmail.com>
2021-09-16 10:46:13 -0400

    Use official build cleaning function

--- pickbc_plus.sh
+++ pickbc_plus.sh
@@ -6,7 +6,7 @@ sudo service memcachedb restart
 npm update
 
 cd ~/rhr/pick
-rm -r build/*
 
+~/rhr/pick/software/build.sh clean
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/api/python/multibot/tools/generate_station_server_code.sh ~/rhr/swagger-codegen
