a7f93377c200dc7cbba1e76b72821a9352bdb23a
Andrew Clough <andrew.clough@gmail.com>
2021-09-30 16:51:53 -0400

    Don't test with 4 threads by default any more

--- pickt.sh
+++ pickt.sh
@@ -2,7 +2,7 @@
 if [ ! $1 ]
 then
     echo "Running with default 4 threads"
-    ~/rhr/pick/software/build.sh runtests -j4
+    ~/rhr/pick/software/build.sh runtests
 else
     echo "Running with $1 threads"
     ~/rhr/pick/software/build.sh runtests -j$1
