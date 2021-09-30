203ade34e86e0eb6ac79c2c95c4b0a40a308f9ab
Andrew Clough <andrew.clough@gmail.com>
2021-09-30 12:12:47 -0400

    Rely on build clean for pickc instead of rm

--- pickbc_plus.sh
+++ pickbc_plus.sh
@@ -5,8 +5,6 @@ set -e
 sudo service memcachedb restart
 npm update
 
-cd ~/rhr/pick
-
 ~/rhr/pick/software/build.sh clean
 ~/rhr/pick/software/build.sh build
 ~/rhr/pick/software/api/python/multibot/tools/generate_station_server_code.sh ~/rhr/swagger-codegen
