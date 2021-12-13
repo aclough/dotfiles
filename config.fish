993a1ac336acc711293fa872dc0e16258de1f9ff
Andrew Clough <andrew.clough@gmail.com>
2021-12-13 15:05:10 -0500

    Some changes brought over from rhr branch
    * Function p to launch python3
    * Update rs to match rhr version
    * Add $argv to ls shortcuts

--- config.fish
+++ config.fish
@@ -37,6 +37,10 @@ function fng
     find -name $argv[1] -print0 2> /dev/null | xargs -0 grep $argv[2]
 end
 
+function p
+    python3 $argv
+end
+
 function a
     rg -S --colors 'path:fg:yellow' $argv
 end
@@ -49,25 +53,25 @@ function vd
     nvim -d $argv
 end
 
+function rs
+    rsync -aurvhP $argv
+end
+
 # Ubuntu .bashrc compatibility
 function ll
-    ls -alFt
+    ls -alFth $argv
 end
 
 function la
-    ls -A
+    ls -A $argv
 end
 
 function l
-    ls -CF
+    ls -CF $argv
 end
 
 function zc
     zig build-exe --library c --c-source $argv
-end
-
-function rs
-    rsync -aurv $argv
 end
 
 function alert
