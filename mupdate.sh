917cc84d7b44f87bd495112eb2dfa0ca7e6de2e4
Andrew Clough <andrew.clough@gmail.com>
2022-06-17 15:07:39 -0400

    Update mupdate
    Inject ssh config and prune docker on mupdates

--- mupdate.sh
+++ mupdate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 cd ~/dotfiles
+bounce --inject-ssh-config
 dconf dump /org/gnome/terminal/ > gnome-terminal-settings.txt
 git pull
 sudo apt update
 sudo apt-get dist-upgrade
+docker system prune
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
 tldr --update
 nvim -c PlugUpdate
