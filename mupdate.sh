4033ca9ad8fe6b71af806bc1d4d15ca40fc09143
Andrew Clough <andrew.clough@gmail.com>
2022-06-17 14:32:16 -0400

    Trim mupdate

--- mupdate.sh
+++ mupdate.sh
@@ -3,15 +3,8 @@ set -euo pipefail
 cd ~/dotfiles
 dconf dump /org/gnome/terminal/ > gnome-terminal-settings.txt
 git pull
-/srv/package-mirror/mirror_pull
 sudo apt update
 sudo apt-get dist-upgrade
-clean-laptop-boot-partition
-sudo service memcachedb restart
-# If something has removed the cuda link regenerate it
-if ! test -L /usr/local/cuda; then
-    sudo apt install --reinstall cuda-toolkit-10-2
-fi
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
 tldr --update
 nvim -c PlugUpdate
