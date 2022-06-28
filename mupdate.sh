8fb8b239fb30f10082fcb442084133924b1bad1c
Andrew Clough <andrew.clough@gmail.com>
2022-06-28 16:06:21 -0400

    Change mupdate docker update command

--- mupdate.sh
+++ mupdate.sh
@@ -6,7 +6,7 @@ dconf dump /org/gnome/terminal/ > gnome-terminal-settings.txt
 git pull
 sudo apt update
 sudo apt-get dist-upgrade
-docker system prune
+docker compose pull
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
 tldr --update
 nvim -c PlugUpdate
