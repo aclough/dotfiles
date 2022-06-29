9bb85b2f8f6a0f57f9428e42e20cb214a234cc95
Andrew Clough <andrew.clough@gmail.com>
2022-06-29 08:34:41 -0400

    Fix typo: docker compose -> docker-compose

--- mupdate.sh
+++ mupdate.sh
@@ -6,7 +6,8 @@ dconf dump /org/gnome/terminal/ > gnome-terminal-settings.txt
 git pull
 sudo apt update
 sudo apt-get dist-upgrade
-docker compose pull
+cd ~/rhr/pick
+docker-compose pull
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
 tldr --update
 nvim -c PlugUpdate
