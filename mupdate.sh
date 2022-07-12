8490c998a6e3e4c0f2e4d9eb22136bbd3df48400
Andrew Clough <andrew.clough@gmail.com>
2022-07-12 15:23:52 -0400

    Use rhr-docker clean intead of direct version

--- mupdate.sh
+++ mupdate.sh
@@ -7,7 +7,7 @@ git pull
 sudo apt update
 sudo apt-get dist-upgrade
 cd ~/rhr/pick
-docker-compose pull
+rhr-docker clean
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
 tldr --update
 nvim -c PlugUpdate
