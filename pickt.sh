70192063fee90f4cacd24e5f942405fa3747c1bb
Andrew Clough <andrew.clough@gmail.com>
2022-07-15 10:04:10 -0400

    Fix borked pickt that also ran non-docker version

--- pickt.sh
+++ pickt.sh
@@ -9,7 +9,6 @@ if [ ! $1 ]
 then
     echo "Running with 1 thread"
     docker-compose exec rightpick ./rightpick/software/build.sh runtests
-    ./software/build.sh runtests
 else
     echo "Running with $1 threads"
     docker-compose exec rightpick ./rightpick/software/build.sh runtests -j$1
