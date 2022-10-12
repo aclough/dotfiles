3e2758eeda307da1e5ec31b1c52745f62efab671
Andrew Clough <aclough@righthandrobotics.com>
2022-10-12 10:36:38 -0400

    Don't bother storing lots of configurations, just switch entirely

--- live-viewer.sh
+++ live-viewer.sh
@@ -13,8 +13,8 @@ docker-compose up -d
 if [[ -n ${bot} ]]
 then
    echo "Copying machine.yaml from ${bot} to ${RIGHTPICK_SOURCE}/default_rightpick_configuration/machine.yaml.${bot}"
-   rsync ${bot}:/configuration/rightpick/machine.yaml ./default_rightpick_configuration/machine.yaml.${bot}
-   COMMAND="${COMMAND}; export RIGHTPICK_MACHINE_CONFIG=/configuration/rightpick/machine.yaml.${bot}"
+   rm -rf ./default_rightpick_configuration/*
+   rsync ${bot}:/configuration/rightpick/* ./default_rightpick_configuration
 fi
 COMMAND="${COMMAND}; LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGL.so live-viewer"
 docker-compose exec rightpick bash -c "${COMMAND}"
