d4ba1c309e4c95f473069f79faf7aad39d8e10ce
Andrew Clough <aclough@righthandrobotics.com>
2023-01-27 13:43:02 -0500

    Change from threads to specific test with pickt option

--- pickt.sh
+++ pickt.sh
@@ -7,9 +7,8 @@ docker-compose up -d
 
 if [ ! $1 ]
 then
-    echo "Running with 1 thread"
     docker-compose exec rightpick ./rightpick/software/build.sh runtests
 else
-    echo "Running with $1 threads"
-    docker-compose exec rightpick ./rightpick/software/build.sh runtests -j$1
+    echo "Running $1 test"
+    docker-compose exec rightpick ./rightpick/software/build.sh runtests -R $1
 fi
