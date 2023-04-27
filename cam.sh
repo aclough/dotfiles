c3fa290fd891513ea43bb73a0abbf918c3c3b537
Andrew Clough <aclough@righthandrobotics.com>
2023-04-27 08:45:23 -0400

    Allow passing in options to can-i-merge

--- cam.sh
+++ cam.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 cd ~/rhr/pick
 docker-compose up -d
 
-docker-compose exec rightpick bash -c "export RIGHTPICK_PLATFORM=v3; ls; source ./rightpick/build/deploy/env.sh; can-i-merge"
+docker-compose exec rightpick bash -c "export RIGHTPICK_PLATFORM=v3; ls; source ./rightpick/build/deploy/env.sh; can-i-merge $1"
 
