ecf03071426e040548596abae1d0d47b649891b3
Andrew Clough <aclough@righthandrobotics.com>
2023-04-27 08:39:59 -0400

    Allow running tests from extended tests

--- pickt.sh
+++ pickt.sh
@@ -10,5 +10,11 @@ then
     docker-compose exec rightpick ./rightpick/software/build.sh runtests
 else
     echo "Running $1 test"
-    docker-compose exec rightpick ./rightpick/software/build.sh runtests -R $1
+    if [[ "$1" == *:* ]];
+    then
+        echo "In extended tests"
+        docker-compose exec rightpick ./rightpick/software/build.sh alltests -R $1
+    else
+        docker-compose exec rightpick ./rightpick/software/build.sh runtests -R $1
+    fi
 fi
