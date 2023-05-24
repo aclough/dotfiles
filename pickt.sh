dd0de3c94caa28751a5dcbeff08a47383854da64
Andrew Clough <aclough@righthandrobotics.com>
2023-05-24 14:32:48 -0400

    Run more specific pytest tests

--- pickt.sh
+++ pickt.sh
@@ -5,6 +5,12 @@ cd ~/rhr/pick
 
 docker-compose up -d
 
+if [ $2 ]
+then
+    echo "Just running $2"
+    export PYTEST_ADDOPTS="-k $2"
+fi
+
 if [ ! $1 ]
 then
     docker-compose exec rightpick ./rightpick/software/build.sh runtests
@@ -15,6 +21,6 @@ else
         echo "In extended tests"
         docker-compose exec rightpick ./rightpick/software/build.sh alltests -R $1
     else
-        docker-compose exec rightpick ./rightpick/software/build.sh runtests -R $1
+        docker-compose exec --env PYTEST_ADDOPTS="${PYTEST_ADDOPTS}" rightpick ./rightpick/software/build.sh runtests -R $1
     fi
 fi
