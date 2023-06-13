97fb368bcd737458c74224ec7c416a575c89d5db
Andrew Clough <andrew.clough@gmail.com>
2023-06-12 21:51:29 -0400

    Python language server in neovim working

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -2,7 +2,7 @@
 
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
+lspconfig.pylsp.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.rust_analyzer.setup {
     -- Server-specific settings. See `:help lspconfig-setup`
