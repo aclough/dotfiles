ecd31d90ab882618d4a90c1aab45a940db00426c
Andrew Clough <andrew.clough@gmail.com>
2023-06-17 11:22:56 -0400

    Adjust language server keybindings
    * Use leader instead of space
    * Format uses leader-T to not step on telescope

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -34,11 +34,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
+        vim.keymap.set('n', '<leader>F', function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
