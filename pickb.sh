b6d4207fbca5ac609443f40d09d4061eab0a5160
Andrew Clough <aclough@righthandrobotics.com>
2023-07-07 10:20:15 -0400

    Automatically update .config permissions after a build if needed

--- pickb.sh
+++ pickb.sh
@@ -7,4 +7,5 @@ cd ~/rhr/pick
 cp -r ~/workspace/flir_venv build
 
 docker-compose up -d
+docker-compose exec rightpick bash -c '[[ $(stat -c %U /home/rhr/.config) == root ]] && sudo chown rhr:rhr /home/rhr/.config || true'
 time docker-compose exec rightpick ./rightpick/software/build.sh build -i
