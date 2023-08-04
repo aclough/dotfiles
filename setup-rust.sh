93f6f41bc02f77b564a070c643c9b3ca87f56f5c
Andrew Clough <andrew.clough@gmail.com>
2023-08-04 07:11:10 -0400

    Racer is now a duplicate

--- setup-rust.sh
+++ setup-rust.sh
@@ -6,8 +6,6 @@ curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 rustup component add rust-src rust-analysis rust-analyzer
 
-cargo install racer # For nvim completion
-
 cargo install tealdeer # for tldr pages
 tldr --update
 
