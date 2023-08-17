21634976c6f45c88d63db9f5032c1b139a8dabcb
Andrew Clough <andrew.clough@gmail.com>
2023-08-17 14:07:42 -0400

    Battle.net dependencies added

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -2,11 +2,22 @@
 
 set -e
 
+# Lutris Battle.net stuff
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt ugprade
+# 32 bit vulkan
+sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
+# Other 32 bit drivers
+sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+# Battle.net dependencies
+
 sudo snap install steam
 
 sudo apt install pipx corectrl
 
 pipx install protonup
+
 
 # Maybe add advanced Radeon drivers and kernel (if this is my desktop)
 # sudo apt-add-repository ppa:oibaf/graphics-drivers
