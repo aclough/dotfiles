d74eb8fedb28e4bc8b35dd1975071289cf12a02c
Andrew Clough <aclough@righthandrobotics.com>
2023-08-29 13:38:37 -0400

    No more need for a flir venv

--- pickb.sh
+++ pickb.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 cd ~/rhr/pick
 
-cp -r ~/workspace/flir_venv build
-
 docker-compose up -d
 docker-compose exec rightpick bash -c '[[ $(stat -c %U /home/rhr/.config) == root ]] && sudo chown rhr:rhr /home/rhr/.config || true'
 time docker-compose exec rightpick ./rightpick/software/build.sh build -i
--- pickbc_plus.sh
+++ pickbc_plus.sh
@@ -7,7 +7,6 @@ docker-compose up -d
 docker-compose exec rightpick bash -c '[[ $(stat -c %U /home/rhr/.config) == root ]] && sudo chown rhr:rhr /home/rhr/.config || true'
 
 docker-compose exec rightpick ./rightpick/software/build.sh clean
-cp -r ~/workspace/flir_venv build
 time docker-compose exec rightpick ./rightpick/software/build.sh build -i
 if [ $1 ]
 then
