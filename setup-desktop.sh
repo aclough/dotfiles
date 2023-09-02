ab52151bed8a6a2093703b142b7af7d3b1e0cef8
Andrew Clough <andrew.clough@gmail.com>
2023-09-02 11:03:16 -0400

    Don't need Lurtris or its deps

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -2,19 +2,9 @@
 
 set -e
 
-# Lutris Battle.net stuff
-sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt ugprade
-# 32 bit vulkan
-sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
-# Other 32 bit drivers
-sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
-# Battle.net dependencies
-
 sudo snap install steam
 
-sudo apt install pipx corectrl
+sudo apt install pipx
 
 pipx install protonup
 
