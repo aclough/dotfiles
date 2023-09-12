1ae4df7b292468351c898de22c2fa9940d576119
Andrew Clough <aclough@righthandrobotics.com>
2023-09-12 09:29:27 -0400

    Update zoom

--- mupdate.sh
+++ mupdate.sh
@@ -4,6 +4,7 @@ cd ~/dotfiles
 git pull
 sudo apt update
 sudo apt-get dist-upgrade
+rhr-setup --update zoom
 cd ~/rhr/pick
 rhr-docker clean
 fish -c "source ~/rhr/pick/build/deploy/env.fish; fish_update_completions"
