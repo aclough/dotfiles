fbd31e7f666c6e67e7a4f12c5dcf28c931abe8ac
Andrew Clough <andrew.clough@gmail.com>
2023-09-17 11:36:56 -0400

    Switch to miniconda

--- setup-conda.sh
+++ setup-conda.sh
@@ -3,8 +3,9 @@
 set -e
 
 cd ~/workspace
-wget https://repo.continuum.io/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
-if [ (sha256sum Anaconda3-2023.07-2-Linux-x86_64.sh) = 75967bc2113d9e336e670e1e557c9198d8b98e59fb9adb82cbe0e71ce5f7c2db]
-    bash Anaconda3-2023.07-2-Linux-x86_64.sh
-    rm Anaconda3-2023.07-2-Linux-x86_64.sh
+FILENAME=Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/$FILENAME
+if [ (sha256sum $FILENAME) = 75967bc2113d9e336e670e1e557c9198d8b98e59fb9adb82cbe0e71ce5f7c2db]
+    bash $FILENAME -b -u -p ~/workspace/anaconda3
+    rm $FILENAME
 end
