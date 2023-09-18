b1dd6299a6d83070645a906ee1b6c3eeff46d1f1
Andrew Clough <andrew.clough@gmail.com>
2023-09-18 07:14:39 -0400

    Fix conda installation script based on tests
    Have to try these things out on another computer

--- setup-conda.sh
+++ setup-conda.sh
@@ -5,7 +5,5 @@ set -e
 cd ~/workspace
 FILENAME=Miniconda3-latest-Linux-x86_64.sh
 wget https://repo.anaconda.com/miniconda/$FILENAME
-if [ (sha256sum $FILENAME) = 75967bc2113d9e336e670e1e557c9198d8b98e59fb9adb82cbe0e71ce5f7c2db]
-    bash $FILENAME -b -u -p ~/workspace/anaconda3
-    rm $FILENAME
-end
+bash $FILENAME -b -p ~/workspace/anaconda3
+rm $FILENAME
