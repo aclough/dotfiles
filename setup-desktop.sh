113c354cc0681fa381656738a2a0b13346f758c1
Andrew Clough <aclough@righthandrobotics.com>
2023-09-19 16:05:36 -0400

    Clean up steam deb

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -8,6 +8,7 @@ sudo apt update
 sudo apt install wget gdebi-core libgl1-mesa-glx:i386
 wget -O ~/steam.deb http://media.steampowered.com/client/installer/steam.deb
 sudo apt install ./steam.deb
+rm steam.deb
 
 sudo apt install pipx
 
