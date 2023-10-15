a1f51acf392ee303685a9d539f04cd47c3251589
Andrew Clough <andrew.clough@gmail.com>
2023-10-15 15:50:32 -0400

    Update conda automatically

--- mupdate.sh
+++ mupdate.sh
@@ -9,6 +9,7 @@ rustup update
 choosenim update stable
 protonup
 cargo install-update -a
+~/workspace/anaconda3/bin/conda update -n base -c defaults conda
 fish -c fish_update_completions
 tldr --update
 nvim --headless "+Lazy! update" +qa
