3cc297cf517b2a32ef3ed00be077d9efeebb8ddd
Andrew Clough <andrew.clough@gmail.com>
2023-10-18 19:04:29 -0400

    Fix typo googgle -> google

---
+++ setup-pytorch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#From https://rocm.docs.amd.com/projects/radeon/en/latest/docs/install/install-pytorch.html
+
+set -e
+
+cd ~/workspace
+pip3 install --upgrade pip wheel
+
+pip install typing_extensions==4.4.0
+
+sudo apt update
+sudo apt install -y git python3-pip python3-venv python3-dev libstdc++-12-dev
+
+curl -O https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/jammy/amdgpu-install_5.7.50701-1_all.deb
+sudo dpkg -i amdgpu-install_5.7.50701-1_all.deb
+rm *.deb
+
+sudo amdgpu-install --usecase=graphics,rocm
+
+sudo usermod -aG video $USER
+sudo usermod -aG render $USER
+
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm5.7
--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 mkdir -p ~/.config/google-chrome/Default
 ln -s ~/dotfiles/chrome_custom_dicionary.txt ~/.config/google-chrome/Default/Custom\ Dictionary.txt
-rm googgle-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 
 # Get Zoom
 wget https://zoom.us/client/5.14.7.2928/zoom_amd64.deb
