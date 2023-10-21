bc12309126b6cfb63e95125e0f3a5f9c7dc7df90
Andrew Clough <andrew.clough@gmail.com>
2023-10-21 14:03:53 -0400

    Add rocminfo too

--- setup-pytorch.sh
+++ setup-pytorch.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-cd ~/workspace
-pip3 install --upgrade pip wheel
-
-pip install typing_extensions==4.4.0
-
 sudo usermod -aG video $USER
 sudo usermod -aG render $USER
 
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm5.7
-
 # RuntimeError: HIP error: invalid device function
 
-sudo pacman -S python-pytorch-rocm
+sudo pacman -S python-pytorch-rocm rocminfo
+
+sudo yay -S python-torchvision-rocm
 
 # fish: Job 1, 'python3 test.py' terminated by signal SIGSEGV (Address boundary error)
