ee72e35cb2359e74e4137417dd92c38aeaea8231
Andrew Clough <andrew.clough@gmail.com>
2023-10-21 13:49:25 -0400

    More setup-desktop improvements

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-pacman -S steam
+pacman -S steam corectrl vulkan-tools
 
 pip install protonup
 
-
-# Maybe add advanced Radeon drivers and kernel (if this is my desktop)
-# But not compatible with steam 32 bit libraries
-# sudo apt-add-repository ppa:oibaf/graphics-drivers
-# sudo add-apt-repository ppa:kisak/kisak-mesa
-# For AMD power management
