170bcbc0011d5da0ff1ff0683b96782fdc0c4654
Andrew Clough <andrew.clough@gmail.com>
2023-10-21 08:53:51 -0400

    Setup updated for EndeavourOS

--- setup.sh
+++ setup.sh
@@ -4,13 +4,11 @@ set -e
 
 # Script to do all the housekeeping stuff I want to do whenever I install Ubuntu on a new computer.
 
-# Neovim unstable
-sudo add-apt-repository ppa:neovim-ppa/unstable
-
 # Utilities and misc
-sudo apt install fish vlc neovim fonts-firacode python3-pip rofi kitty \
-ripgrep curl variety calibre clangd libssl-dev gnome-tweaks neofetch \
-shellcheck sox black python3-pylsp python3-neovim fzy
+sudo pacman -S fish neovim vlc python-pip rofi kitty ripgrep curl variety \
+	calibre clang neofetch shellcheck sox fzy python-lsp-server python-pynvim \
+	base-devel zig
+
 # shellcheck:  For neovim checking
 # sox:  For the `play` command
 # black:  Python formatter
@@ -20,18 +18,7 @@ shellcheck sox black python3-pylsp python3-neovim fzy
 # fzy:  For neovim file/buffer/etc finding
 # python3-pydrive2: For deja-dup
 
-
-# Get Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-mkdir -p ~/.config/google-chrome/Default
-ln -s ~/dotfiles/chrome_custom_dicionary.txt ~/.config/google-chrome/Default/Custom\ Dictionary.txt
-rm google-chrome-stable_current_amd64.deb
-
-# Get Zoom
-wget https://zoom.us/client/5.14.7.2928/zoom_amd64.deb
-sudo apt install ./zoom_amd64.deb
-rm zoom_amd64.deb
+yay -S google-chrome
 
 # Don't use touchpad if I've got another pointer availabile
 gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
@@ -40,18 +27,8 @@ gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
 # Swap caplocks and escape
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 
-# Revert nautilus to previous default search
-sudo add-apt-repository ppa:lubomir-brindza/nautilus-typeahead
-
-# Enable DVDs
-sudo apt-get install libdvd-pkg
-sudo dpkg-reconfigure libdvd-pkg
-
 # Nim
 curl https://nim-lang.org/choosenim/init.sh -sSf | sh
-
-# Zig
-snap install zig --classic --beta
 
 # Font
 mkdir ~/.fonts
