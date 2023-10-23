2331ee2a192a6aabebc656ffa23e3ecbb520a86a
Andrew Clough <aclough@righthandrobotics.com>
2023-10-23 11:12:36 -0400

    Include useful scripts

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 pickb.sh
 echo "Built successfully"
+cp ~/workspace/setup.sh ~/rhr/pick/build
+cp ~/workspace/env.sh ~/rhr/pick/build
+cp ~/workspace/pick.sh ~/rhr/pick/build
 for bot in "$@"
 do
     rsync -raluhP ~/rhr/pick/build rhr@"$bot":aclough
