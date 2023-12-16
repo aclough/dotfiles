ab0624cf44636dcef54202c1d63840374ce4bfbc
Andrew Clough <andrew.clough@gmail.com>
2023-12-16 08:18:22 -0500

    Link to rocm instructions

--- setup-pytorch.sh
+++ setup-pytorch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+#https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Install-and-Run-on-AMD-GPUs
 
 set -e
 
 sudo usermod -aG video $USER
 sudo usermod -aG render $USER
 
-# RuntimeError: HIP error: invalid device function
 
 sudo pacman -S python-pytorch-rocm rocminfo
 
-sudo yay -S python-torchvision-rocm
-
-# fish: Job 1, 'python3 test.py' terminated by signal SIGSEGV (Address boundary error)
+sudo yay -Sns python-torchvision-rocm
