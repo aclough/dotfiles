a5ce6590f0c513ae36fe61649694c2fd4e8ab874
Andrew Clough <aclough@righthandrobotics.com>
2024-04-30 08:28:08 -0400

    Install webp converter and add instructions in comment

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Note: Config at /home/aclough/.config/rclone/rclone.conf"
+# To convert webps, dwebp Filename.webp -o Filename.jpg
 
 echo "Backing up Wallapers"
 rclone copy -P ~/Pictures/Wallpapers gdrive:backup/Pictures/Wallpapers --update
