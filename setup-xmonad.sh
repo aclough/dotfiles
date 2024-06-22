956168d5cc9315862d278541028f6bc228bd9f00
Andrew Clough <andrew.clough@gmail.com>
2024-06-22 06:44:12 -0400

    Fix default xmonad layout change
    Gnome grabs super-space for input source shifting, so have the xmonad
    setup script move that keybinding to include a ctrl.
    
    Also do the same for the reverse keybinding

--- setup-xmonad.sh
+++ setup-xmonad.sh
@@ -9,6 +9,10 @@ ln -s ~/dotfiles/xmonad.hs ~/.xmonad
 xmonad --recompile
 ln -s ~/dotfiles/xmonad.desktop ~/.config/autostart
 
+# Don't let input setting override switching layouts
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Control><Super>space']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Control><Super>space']"
+
 echo "#########################"
 echo "Remember to log in via the metacity falshback session"
 echo "#########################"
