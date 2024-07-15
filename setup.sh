318202605993faf13a4feaaaef317336d78b6d5c
Andrew Clough <andrew.clough@gmail.com>
2024-07-15 12:12:35 -0400

    Add boost::program_options to setup

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo apt dist-upgrade
 sudo apt install -y vlc fonts-firacode python3-pip \
 ripgrep fd-find curl variety calibre clangd libssl-dev gnome-tweaks neofetch \
 shellcheck sox black python3-pylsp python3-neovim fzy git pkg-config libssl-dev \
-naev tig usb-creator-gtk gparted nvtop nodejs
+naev tig usb-creator-gtk gparted nvtop nodejs libboost-program-options-dev
 # shellcheck:  For neovim checking
 # sox:  For the `play` command
 # black:  Python formatter
@@ -26,6 +26,7 @@ naev tig usb-creator-gtk gparted nvtop nodejs
 # gparted: Edit partitions
 # nvtop: Monitor GPU usage
 # nodejs: For Github copilot
+# libboost-program-options-dev: For C++ argparse
 
 # Optional tools I want to record here:
 # horizon-eda: PCB layout tool
