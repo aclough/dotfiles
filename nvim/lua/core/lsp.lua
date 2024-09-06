32d8c752a470d11dbe40da0b71e83542e6d41d04
Andrew Clough <andrew.clough@gmail.com>
2024-09-06 15:59:50 -0400

    Rename tsserver to ts_ls
    Apparently they renamed it and the old version gives a deprecation
    warning on startup.

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -14,7 +14,7 @@ lspconfig.pylsp.setup {
     }
   }
 }
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.rust_analyzer.setup {
     -- Server-specific settings. See `:help lspconfig-setup`
     settings = {
