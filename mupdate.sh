ee929d2dbbec0a2487535d0dc7acbb491afec20f
Andrew Clough <andrew.clough@gmail.com>
2025-04-07 17:30:10 -0400

    Call prtonup from conda

--- mupdate.sh
+++ mupdate.sh
@@ -6,10 +6,11 @@ sudo apt update
 sudo apt-get dist-upgrade
 set +e
 rustup update
-choosenim update stable
-protonup
 cargo install-update -a
-~/workspace/conda/bin/conda update -n base -c defaults conda
+choosenim update stable
+source ~/workspace/conda/bin/activate
+protonup
+conda update -n base -c defaults conda
 fish -c fish_update_completions
 tldr --update
 nvim --headless "+Lazy! update" +qa
