ea6691309d76a727ba59be94769481960a23d5d0
Andrew Clough <andrew.clough@gmail.com>
2025-05-11 19:36:28 -0400

    Smaller rocm install compatible with 25.04

--- setup-pytorch.sh
+++ setup-pytorch.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-sudo apt install rocminfo clinfo libamd-comgr2 libamdhip64-5 libhipblas0 libhipfft0 libhiprand1 libhipsolver0 \
-    libhipsparse0 libhsa-runtime64-1 librccl1 librocalution0 librocblas0 librocfft0 librocm-smi64-1 librocrand1 \
-    librocsolver0 librocsparse0 rocm-device-libs-17 rocm-smi hipcc
+sudo apt install hipcc rocm-smi rocminfo clinfo
 
 sudo usermod -aG render,video $USER
 
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.0
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.1
