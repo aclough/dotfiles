e419615e7519d5d6c8ac8752f3523661126c6c97
Andrew Clough <andrew.clough@gmail.com>
2025-08-24 13:01:51 -0400

    Add poetry

--- setup-pytorch.sh
+++ setup-pytorch.sh
@@ -7,3 +7,6 @@ sudo apt install hipcc rocm-smi rocminfo clinfo
 sudo usermod -aG render,video $USER
 
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.1
+
+# In global scope
+pipx install poetry
