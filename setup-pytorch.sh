be90c885cfbdc70ad29612b55c64f3acf4c66eab
Andrew Clough <andrew.clough@gmail.com>
2025-11-01 18:33:09 -0400

    Revise things to use ROCM 7.1 by official method

--- setup-pytorch.sh
+++ setup-pytorch.sh
@@ -4,20 +4,52 @@
 
 set -e
 
-cd ~/workspace
-pip3 install --upgrade pip wheel
-
-pip install typing_extensions==4.4.0
-
-sudo apt update
-sudo apt install -y git python3-pip python3-venv python3-dev libstdc++-12-dev
-
-curl -O https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/jammy/amdgpu-install_5.7.50701-1_all.deb
-sudo dpkg -i amdgpu-install_5.7.50701-1_all.deb
-rm *.deb
-
-sudo amdgpu-install --usecase=rocm
-
 sudo usermod -aG render,video $USER
 
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm5.7
+sudo apt install environment-modules
+
+# Make the directory if it doesn't exist yet.
+# This location is recommended by the distribution maintainers.
+sudo mkdir --parents --mode=0755 /etc/apt/keyrings
+
+# Download the key, convert the signing-key to a full
+# keyring required by apt and store in the keyring directory
+wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
+    gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+
+sudo tee /etc/apt/sources.list.d/rocm.list << EOF
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/7.1 noble main
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/graphics/7.1/ubuntu noble main
+EOF
+
+sudo tee /etc/apt/preferences.d/rocm-pin-600 << EOF
+Package: *
+Pin: release o=repo.radeon.com
+Pin-Priority: 600
+EOF
+
+sudo apt update
+sudo apt install rocm
+￼
+# Kernel modules and drivers
+sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+sudo apt install amdgpu-dkms
+
+
+# Configure ROCm shared objects
+sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
+/opt/rocm/lib
+/opt/rocm/lib64
+EOF
+sudo ldconfig
+
+# Set library path (crucial for multi-version installs)
+export LD_LIBRARY_PATH=/opt/rocm-7.0.0/lib
+
+# Install OpenCL runtime
+sudo apt install rocm-opencl-runtime
+
+uv venv
+source .venv/bin/activate
+# This will be cached and almost instant when run on future projects
+uv pip install torch torchvision --find-links https://repo.radeon.com/rocm/manylinux/rocm-rel-7.1/
