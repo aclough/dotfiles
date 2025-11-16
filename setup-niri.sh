346943ab71735b6be839e9caea017dfc4889e579
Andrew Clough <andrew.clough@gmail.com>
2025-11-16 11:48:33 -0500

    Add configuration

--- setup-niri.sh
+++ setup-niri.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Dependencies
+# Dependencies for build
 sudo apt-get install -y gcc clang libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libpipewire-0.3-dev libpango1.0-dev libdisplay-info-dev
 
 cd ~/workspace
@@ -17,3 +17,9 @@ sudo cp resources/niri.desktop /usr/share/wayland-sessions
 sudo cp resources/niri-portals.conf /usr/share/xdg-desktop-portal/
 sudo cp resources/niri.service /etc/systemd/user/
 sudo cp resources/niri-shutdown.target /etc/systemd/user/
+
+# For config
+sudo apt-get install -y swaylock fuzzel brightnessctl
+
+mkdir -p ~/.config/niri
+ln -s ~/dotfiles/niri_config.kdl ~/.config/niri/config.kdl
