888514e65005b4b401b20ef84bc1dadfd1ad9604
Andrew Clough <andrew.clough@gmail.com>
2025-11-22 13:44:38 -0500

    Add xwayland-satellite

--- setup-wayland.sh
+++ setup-wayland.sh
@@ -13,3 +13,12 @@ cargo install wpaperd
 
 mkdir -p ~/.config/wpaperd
 ln -s ~/dotfiles/wpaperd-config.toml ~/.config/wpaperd/config.toml
+
+
+sudo apt install -y libxcb-composite0-dev libxcb-res0-dev libwayland-dev pkg-config libxcb-cursor-dev
+cd ~/workspace
+git clone https://github.com/Supreeeme/xwayland-satellite
+cd xwayland-satellite
+cargo build --release
+# We want this system-wide
+sudo cp target/release/xwayland-satellite /usr/local/bin
