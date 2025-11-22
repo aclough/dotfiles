85d8a9482bca10a88d7a8a62841a88c5c7ac4fd6
Andrew Clough <andrew.clough@gmail.com>
2025-11-22 13:13:15 -0500

    Add wallpaper deamoon
    Also don't spawn an extra copy of waybar, it's already in systemd

--- setup-waybar.sh
+++ setup-waybar.sh
@@ -6,3 +6,8 @@ sudo apt-get install -y swaylock fuzzel brightnessctl waybar pavucontrol thunar 
 
 mkdir -p ~/.config/waybar
 ln -s ~/dotfiles/waybar-config.jsonc ~/.config/waybar/config.jsonc
+
+cargo install wpaperd
+
+mkdir -p ~/.config/wpaperd
+ln -s ~/dotfiles/wpaperd-config.toml ~/.config/wpaperd/config.toml
