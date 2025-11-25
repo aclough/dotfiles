73aa53b772710769cbc4b741ee861f9dd22a5491
Andrew Clough <andrew.clough@gmail.com>
2025-11-25 16:59:55 -0500

    Waybar related updates
    * Use wlogout for power icon
    * New clock date format
    * Show idle icons in tray
    * Move clock to middle
    * Use impala for network management
        But I've got a nm-applet instead

--- setup-wayland.sh
+++ setup-wayland.sh
@@ -7,12 +7,12 @@ set -e
 # Wayland fundamentals and build fundamentals
 sudo apt-get install -y gcc clang libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libpipewire-0.3-dev libpango1.0-dev libdisplay-info-dev
 
-sudo apt-get install -y swaylock fuzzel brightnessctl waybar pavucontrol thunar xdg-desktop-portal-gnome wl-clipboard
+sudo apt-get install -y swaylock fuzzel brightnessctl waybar pavucontrol thunar xdg-desktop-portal-gnome wl-clipboard wlogout
 
 mkdir -p ~/.config/waybar
 ln -s ~/dotfiles/waybar-config.jsonc ~/.config/waybar/config.jsonc
 
-cargo install wpaperd
+cargo install wpaperd impala
 
 mkdir -p ~/.config/wpaperd
 ln -s ~/dotfiles/wpaperd-config.toml ~/.config/wpaperd/config.toml
