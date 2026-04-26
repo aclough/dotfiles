101675492a2019899ca418c7e0724870caba6915
Andrew Clough <andrew.clough@gmail.com>
2026-04-26 07:15:30 -0400

    mako -> mako-notifier

--- setup-niri.sh
+++ setup-niri.sh
@@ -5,7 +5,7 @@ set -e
 cd ~/dotfiles
 ./setup-wayland.sh
 
-sudo apt install -y nemo mako
+sudo apt install -y nemo mako-notifier
 
 # Dependencies for build
 sudo apt-get install -y gcc clang libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libpipewire-0.3-dev libpango1.0-dev libdisplay-info-dev mate-polkit
